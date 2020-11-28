f4046d2b1946097b7f7a41abd28f20279345a649
yutake2000 <yutake2000@gmail.com>
2020-11-29 00:37:52 +0900

    my_bouncing2.cを追加

---
+++ soft2-201126-src/my_bouncing2.h
@@ -0,0 +1,45 @@
+// シミュレーション条件を格納する構造体
+// 反発係数CORを追加
+typedef struct condition
+{
+  const int width; // 見えている範囲の幅
+  const int height; // 見えている範囲の高さ
+  const double G; // 重力定数
+  const double dt; // シミュレーションの時間幅
+  const double cor; // 壁の反発係数
+} Condition;
+
+// 個々の物体を表す構造体
+typedef struct object
+{
+  double m;
+  double y, x;
+  double prev_y, prev_x; // 壁からの反発に使用
+  double vy, vx;
+} Object;
+
+// 授業で用意した関数のプロトタイプ宣言
+/*
+void plot_objects(Object objs[], const size_t numobj, const double t, const Condition cond);
+void update_velocities(Object objs[], const size_t numobj, const Condition cond);
+void update_positions(Object objs[], const size_t numobj, const Condition cond);
+void bounce(Object objs[], const size_t numobj, const Condition cond);
+*/
+
+// To do: 以下に上記と同じ引数で実行される my_* を作成
+// 実装できたらmain関数で上記と入れ替えていく
+// my_plot_objects(), my_update_velocities(), my_update_positions(), my_bounce の4つ 
+
+int my_plot_objects(Object objs[], const size_t numobj, const double t, const Condition cond);
+void my_update_velocities(Object objs[], const size_t numobj, const Condition cond);
+void my_update_positions(Object objs[], const size_t numobj, const Condition cond);
+void my_bounce(Object objs[], const size_t numobj, const Condition cond);
+
+// 座標が画面内にあるかどうか判定する
+int in_screen(double y, double x, const Condition cond);
+
+// a, b, cがこの順に単調増加または単調減少であるかどうかを判定する
+int is_monotonic(double a, double b, double c);
+
+// オブジェクトファイルを読み込む
+void load_objects(int numobj, Object objs[], char filename[], const Condition cond);
